d5d58afbe4a8fe9d68ec220874146ed04bc41a5c
victoricoma <victorsena3010@gmail.com>
2023-02-09 20:21:09 -0300

    Finalização da Aula0602 com template string

--- Aula0602/index.js
+++ Aula0602/index.js
@@ -1,1 +1,11 @@
+var e=0;
+var f="Lindao";
+var g=4.25;
+var h=[23, 14, 25]
+var k={nome:"Jose Nelson", profisao:"Barbeiro"}
+
 const meuModulo = require('./soma')
+meuModulo.calculadora(125, 25, "+");
+meuModulo.calculadora(125, 25, "-");
+meuModulo.calculadora(125, 25, "*");
+meuModulo.calculadora(125, 25, "/");
